2033ab85c8e1c07945a4f3cd0993778c81a21a38
Rafał Pyżalski <rafal.pyzalski08@gmail.com>
2020-09-09 22:56:08 +0200

    getting ball moving

--- objects.py
+++ objects.py
@@ -1,6 +1,11 @@
 import pygame
 
-class Player():
+# window sizes
+HEIGHT = 800
+WIDTH = 1280
+
+
+class Player:
     def __init__(self, x, y, width, height, color):
         self.x = x
         self.y = y
@@ -13,24 +18,44 @@ class Player():
     def draw(self, window):
         pygame.draw.rect(window, self.color, self.rect)
 
-    # def move(self):
-    #     keys = pygame.key.get_pressed()
-    #     if keys[pygame.K_UP]:
-    #         self.y -= self.vel
-    #     if keys[pygame.K_DOWN]:
-    #         self.y += self.vel
-    #
+    def move(self):
+        '''
+        moving object up and down with keys
+        '''
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and self.y > 10:
+            self.y -= self.vel
+        if keys[pygame.K_DOWN] and self.y < (HEIGHT - (10 + self.height)):
+            self.y += self.vel
+
+        self.update()
+
+    def update(self):
+        '''
+        update object after moving it
+        '''
+        self.rect = (self.x, self.y, self.width, self.height)
 
 
 # ball object
-class Ball():
+class Ball:
     def __init__(self, x, y, width, height, color):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.color = color
-        self.rect = (x, y, width, height)
+        self.ellipse = (x, y, width, height)
+        self.vel_x = 5
+        self.vel_y = 5
 
     def draw(self, window):
-        pygame.draw.rect(window, self.color, self.rect)
+        pygame.draw.ellipse(window, self.color, self.ellipse)
+
+    def move(self):
+        self.x +=self.vel_x
+        self.y +=self.vel_y
+        self.update()
+
+    def update(self):
+        self.ellipse = (self.x, self.y, self.width, self.height)
--- pong.py
+++ pong.py
@@ -7,22 +7,22 @@ clock = pygame.time.Clock()
 # game constants
 FPS = 60
 WIDTH = 1280  # screen width
-HEIGHT = 960  # screen height
+HEIGHT = 800  # screen height
 
 # main window
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Pong:The Game')
 
-
-white = (255,255,255)
+white = (255, 255, 255)
 # creating game obj
 ball = Ball(WIDTH / 2 - 15, HEIGHT / 2 - 15, 30, 30, white)
-player = Player(WIDTH - 20, HEIGHT/2 - 70, 10, 140, white)
-opponent = Player(10, HEIGHT/2 - 70, 10, 140, white)
+player = Player(WIDTH - 20, HEIGHT / 2 - 70, 10, 120, white)
+opponent = Player(10, HEIGHT / 2 - 70, 10, 120, white)
 
 
-def redrawWindow(win, b, p1, p2):
-    win.fill((51, 51, 51))
+def redrawWindow(b, p1, p2):
+    WINDOW.fill((51, 51, 51))
+    pygame.draw.aaline(WINDOW, white, (WIDTH/2, 0), (WIDTH/2, HEIGHT) )
     b.draw(WINDOW)
     p1.draw(WINDOW)
     p2.draw(WINDOW)
@@ -40,7 +40,9 @@ def main():
                 run = False
                 pygame.quit()
 
-        redrawWindow(WINDOW, ball, player, opponent)
+        player.move()
+        ball.move()
+        redrawWindow(ball, player, opponent)
 
 
 main()
